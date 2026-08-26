317837bf0e61da942060a0f476ae4b385e220f9c
Shreyasrp07 <3shreyas2007@gmail.com>
2026-08-26 19:26:41 +0530

    Position shared Dex background above page surfaces

--- frontend/src/components/DexBackground.tsx
+++ frontend/src/components/DexBackground.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const DexBackground: React.FC = () => (
   <>
-    <div className="pointer-events-none fixed inset-0 z-0 overflow-hidden" aria-hidden="true">
+    <div className="pointer-events-none fixed inset-0 z-[1] overflow-hidden" aria-hidden="true">
       <svg className="absolute -left-[20%] -top-[8%] h-[116%] w-[140%]" viewBox="0 0 1200 700" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
         <path d="M-700 590C-250 120 120 40 560 190C1000 340 1370 570 2050 150" stroke="#7C3AED" strokeWidth="1.2" strokeLinecap="round" opacity="0.19"><animateTransform attributeName="transform" type="translate" values="-220 90;220 -90;-220 90" dur="12s" repeatCount="indefinite" /></path>
         <path d="M-750 210C-280 650 160 650 610 360C1050 75 1450 120 2050 520" stroke="#A855F7" strokeWidth="0.9" strokeLinecap="round" opacity="0.17"><animateTransform attributeName="transform" type="translate" values="220 -110;-220 110;220 -110" dur="16s" repeatCount="indefinite" /></path>
