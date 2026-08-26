d70485bc9bcb6b8ce3acd97773e67fa36a3e251f
Shreyasrp07 <3shreyas2007@gmail.com>
2026-08-26 15:16:36 +0530

    Extend ambient vector lines beyond viewport

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -4,10 +4,10 @@ export const LandingPage: React.FC = () => {
   return (
     <div className="relative min-h-screen w-full overflow-hidden bg-[#080810] flex flex-col">
       <div className="pointer-events-none absolute inset-0 z-0 overflow-hidden" aria-hidden="true">
-        <svg className="absolute -left-[12%] top-[12%] h-[76%] w-[124%] opacity-[0.16]" viewBox="0 0 1200 700" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path className="dex-orbit dex-orbit-one" d="M-80 520C120 150 330 80 560 190C790 300 900 620 1280 280" stroke="#7C3AED" strokeWidth="1.2" strokeLinecap="round" />
-          <path className="dex-orbit dex-orbit-two" d="M-120 300C170 570 390 580 610 360C820 150 1010 160 1300 430" stroke="#A855F7" strokeWidth="0.9" strokeLinecap="round" />
-          <path className="dex-orbit dex-orbit-three" d="M40 680C260 390 470 330 700 450C930 570 1080 520 1250 170" stroke="#2DD4BF" strokeWidth="0.7" strokeLinecap="round" />
+        <svg className="absolute -left-[20%] -top-[8%] h-[116%] w-[140%] opacity-[0.16]" viewBox="0 0 1200 700" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path className="dex-orbit dex-orbit-one" d="M-700 590C-250 120 120 40 560 190C1000 340 1370 570 2050 150" stroke="#7C3AED" strokeWidth="1.2" strokeLinecap="round" />
+          <path className="dex-orbit dex-orbit-two" d="M-750 210C-280 650 160 650 610 360C1050 75 1450 120 2050 520" stroke="#A855F7" strokeWidth="0.9" strokeLinecap="round" />
+          <path className="dex-orbit dex-orbit-three" d="M-500 850C-100 420 300 280 700 450C1080 610 1450 420 1900 -20" stroke="#2DD4BF" strokeWidth="0.7" strokeLinecap="round" />
           <circle className="dex-particle dex-particle-one" cx="260" cy="220" r="2" fill="#A855F7" />
           <circle className="dex-particle dex-particle-two" cx="850" cy="470" r="1.5" fill="#2DD4BF" />
           <circle className="dex-particle dex-particle-three" cx="1020" cy="230" r="1.5" fill="#A855F7" />
