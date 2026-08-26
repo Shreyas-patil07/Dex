4d035f8c38bd405d3b2e148b742c55d51258b1f1
Shreyasrp07 <3shreyas2007@gmail.com>
2026-08-26 14:50:41 +0530

    Wire legal policy routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import { LandingPage } from './pages/LandingPage';
+import { PrivacyPolicy } from './pages/PrivacyPolicy';
+import { TermsOfService } from './pages/TermsOfService';
 
 export const App: React.FC = () => {
+  const path = window.location.pathname.replace(/\/$/, '') || '/';
+
+  if (path === '/privacy-policy') return <PrivacyPolicy />;
+  if (path === '/terms-of-service') return <TermsOfService />;
+
   return <LandingPage />;
 };
 
