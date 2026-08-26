e51e38c314d756213582a667f70d0491a9a049e7
Shreyasrp07 <3shreyas2007@gmail.com>
2026-08-26 15:30:08 +0530

    Make Dex root page trending

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,16 @@ import React from 'react';
 import { LandingPage } from './pages/LandingPage';
 import { PrivacyPolicy } from './pages/PrivacyPolicy';
 import { TermsOfService } from './pages/TermsOfService';
+import { TrendingPage } from './pages/TrendingPage';
 
 export const App: React.FC = () => {
   const path = window.location.pathname.replace(/\/$/, '') || '/';
 
   if (path === '/privacy-policy') return <PrivacyPolicy />;
   if (path === '/terms-of-service') return <TermsOfService />;
-  if (path === '/' || path === '/about-me') return <LandingPage />;
+  if (path === '/about-me') return <LandingPage />;
 
-  return <LandingPage />;
+  return <TrendingPage />;
 };
 
 export default App;
